08b51fd8fae0d0a6c6a355ae2b55495a5950bbce
codergrl-Mayuri <128097115+codergrl-Mayuri@users.noreply.github.com>
2023-10-05 01:13:29 +0530

    Add files via upload

--- layout.tsx
+++ layout.tsx
@@ -1,13 +1,35 @@
-const AuthLayout = ({
-  children
-}: {
-  children: React.ReactNode;
-}) => {
-  return ( 
-    <main className="h-full bg-[#111827] flex items-center justify-center">
-      {children}
-    </main>
-  );
+import type { Metadata } from 'next'
+import { Inter } from 'next/font/google'
+import { ClerkProvider } from '@clerk/nextjs'
+
+import { ToasterProvider } from '@/components/toaster-provider'
+import { ModalProvider } from '@/components/modal-provider'
+import { CrispProvider } from '@/components/crisp-provider'
+
+import './globals.css'
+
+const font = Inter({ subsets: ['latin'] });
+
+export const metadata: Metadata = {
+  title: 'Genius',
+  description: 'AI Platform',
 }
- 
-export default AuthLayout;
+
+export default async function RootLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return (
+    <ClerkProvider>
+      <html lang="en" suppressHydrationWarning>
+        <CrispProvider />
+        <body className={font.className}>
+          <ToasterProvider />
+          <ModalProvider />
+          {children}
+        </body>
+      </html>
+    </ClerkProvider>
+  )
+}
